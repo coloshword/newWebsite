d2c58df73a2b79f96a712b8d17f97fa452b78455
coloshword <aceliang2001@gmail.com>
2021-02-12 21:06:40 -0500

    Changed the layout

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let body = "";
 
-async function sampleFunc(url, meal) {
+async function sampleFunc(url, meal, classModifier) {
     const response = await fetch(url, {
       headers: {
         "Content-Type": "application/json",
@@ -9,7 +9,7 @@ async function sampleFunc(url, meal) {
     });
     body = await response.json();
     console.log(body);
-    displayFood(body, meal)
+    displayFood(body, meal, classModifier)
 }
 
 function sleepFor( sleepDuration ){
@@ -18,17 +18,18 @@ function sleepFor( sleepDuration ){
 }
 
 
-sampleFunc('http://172.24.49.229:8080/breakfast', "Breakfast");
-sleepFor(100);
-sampleFunc('http://172.24.49.229:8080/lunch', "Lunch");
-sleepFor(100);
-sampleFunc('http://172.24.49.229:8080/dinner', "Dinner");
+sampleFunc('http://172.24.49.229:8080/breakfast', "Breakfast", 0);
+sleepFor(150);
+sampleFunc('http://172.24.49.229:8080/lunch', "Lunch", 1);
+sleepFor(150);
+sampleFunc('http://172.24.49.229:8080/dinner', "Dinner", 2);
 
 
-function displayFood(dict, meal) {
-    var addText = document.querySelector(".putFood");
+function displayFood(dict, meal, classModifier) {
+    var addText = document.querySelector(".putFood" + classModifier);
     let h4 = document.createElement("h4");
-    h4.setAttribute("style", "text-align: center; color: rgb(235, 155, 76)");
+    let color = ["(235, 155, 76)", "(201, 71, 245)", "(79,134,247)"];
+    h4.setAttribute("style", "text-align: center; color: rgb" + color[classModifier]);
     h4.appendChild(document.createTextNode("<<" + meal + ">>"));
     addText.appendChild(h4);
     var foodContainer = document.createElement("div");
@@ -39,7 +40,7 @@ function displayFood(dict, meal) {
         let displayBox = document.createElement("div");
         displayBox.classList = "displayBox"
         let ribbon = document.createElement("div");
-        ribbon.classList = "topRibbon";
+        ribbon.classList = ("topRibbon" + classModifier);
         let h6 = document.createElement("h6");
         h6.setAttribute("style", "text-align: center;")
         h6.appendChild(document.createTextNode(item.item));
@@ -48,35 +49,3 @@ function displayFood(dict, meal) {
         foodContainer.appendChild(displayBox);
     }
 }
-
-
-// function displayFood(meal, dict) {
-//     var mainContainer = document.querySelector(".putFood");
-//     let h4 = document.createElement("h4");
-//     h4.setAttribute("style", "text-align: center; color: rgb(235, 155, 76)");
-//     h4.appendChild(document.createTextNode("<<" + meal + ">>"));
-//     var textBox = document.createElement("div");
-//     textBox.classList.add("textBox");
-//     textBox.appendChild(h4);
-//     mainContainer.appendChild(textBox);
-//     // create the div containing the input boxes
-//     var inputBox = document.createElement("div");
-//     inputBox.classList.add("inputBox")
-//     mainContainer.appendChild(inputBox);
-//     for(var key in dict[key]) {
-//         item = dict[key];
-//         let displayBox = document.createElement("div");
-//         displayBox.classList.add("displayBox");
-//         let ribbon = document.createElement("div");
-//         ribbon.classList.add("topRibbon");
-//         let h6 = document.createElement("h6");
-//         h6.setAttribute("style", "text-align: center;");
-//         h6.appendChild(document.createTextNode(key + "\n" + item));
-//         displayBox.appendChild(ribbon);
-//         displayBox.appendChild(h6);
-//         inputBox.appendChild(displayBox);
-//         console.log("Upt to here")
-//     }
-// }
-
-//displayFood(foods, "Breakfast")
